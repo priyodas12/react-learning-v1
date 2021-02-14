92c41856fdd3f742743432989365885d42fbe52d
Priyobrato Das <priyodas12@gmail.com>
2021-02-14 12:51:30 +0530

    change-004

--- src/Human/Human.js
+++ src/Human/Human.js
@@ -3,7 +3,7 @@ import React from "react";
 const human = () => {
   return (
     <div className="human">
-      <h4>Human Being!</h4>
+      <h4>`Human Being with IQ Level {Math.floor(Math.random() * 80)}/100`</h4>
     </div>
   );
 }
