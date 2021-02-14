e557fa5cbedf628f51932b4ca1eff95510aeb555
Priyobrato Das <priyodas12@gmail.com>
2021-02-14 16:58:50 +0530

    change-006

--- src/App.js
+++ src/App.js
@@ -2,14 +2,49 @@
 import './App.css';
 import Human from './Human/Human'
 
-function App() {
-  return (
-    <div className="App">
-      <Human name="A" age="54"></Human>
-      <Human name="B" age="34" ></Human >
-      <Human name="C" age="23" ></Human >
-    </div >
-  );
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      humans: [
+        { name: "A", age: 24 },
+        { name: "B", age: 44 },
+        { name: "C", age: 84 }
+      ]
+    }
+
+    this.handleEvent = this.handleEvent.bind(this)
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) { if (prevState.name !== this.state.name) { this.handler() } }
+
+  componentWillUnmount() {
+
+  }
+
+  // Prototype methods, Bind in Constructor (ES2015)
+  handleEvent() { }
+
+  // Class Properties (Stage 3 Proposal)
+  handler = () => { this.setState() }
+
+  render() {
+    return (
+      <>
+        <Human name={this.state.humans[0].name} age={this.state.humans[0].age}></Human>
+        <Human name={this.state.humans[1].name} age={this.state.humans[1].age}></Human>
+        <Human name={this.state.humans[2].name} age={this.state.humans[2].age}></Human>
+      </>
+    )
+  }
 }
 
-export default App;
--- src/Human/Human.js
+++ src/Human/Human.js
@@ -4,6 +4,7 @@ const human = (props) => {
   return (
     <div className="human">
       <h4>'{props.name} with IQ Level {Math.floor(Math.random() * props.age)}/100`</h4>
+      <p>{props.children}</p>
     </div>
   );
 }
