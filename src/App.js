ceebfc3a431d75a426991f292cf4eba8041d4ca3
Priyobrato Das <priyodas12@gmail.com>
2021-02-14 17:50:09 +0530

    change-008

--- src/App.js
+++ src/App.js
@@ -1,61 +1,95 @@
 
 import './App.css';
+
+// import React, { Component } from 'react'
+//import Human from './Human/Human'
+// export default class App extends Component {
+
+
+//   constructor(props) {
+//     super(props)
+
+//     this.state = {
+//       humans: [
+//         { name: "A", age: 24 },
+//         { name: "B", age: 44 },
+//         { name: "C", age: 84 }
+//       ]
+//     }
+
+//     this.handleEvent = this.handleEvent.bind(this)
+//     console.log(this.props)
+//     console.log(this.state)
+//   }
+
+//   componentDidMount() {
+
+//   }
+
+//   componentDidUpdate(prevProps, prevState, snapshot) { if (prevState.name !== this.state.name) { this.handler() } }
+
+//   componentWillUnmount() {
+
+//   }
+
+//   // Prototype methods, Bind in Constructor (ES2015)
+//   handleEvent() { }
+
+//   // Class Properties (Stage 3 Proposal)
+//   iqChnageHandler = (e) => {
+//     console.log(e.type, e.target)
+//     this.setState(
+//       {
+//         humans: [
+//           { name: "A", age: 50 },
+//           { name: "B", age: 50 },
+//           { name: "C", age: 50 }
+//         ]
+//       }
+//     )
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <Human name={this.state.humans[0].name} age={this.state.humans[0].age}></Human>
+//         <Human name={this.state.humans[1].name} age={this.state.humans[1].age}></Human>
+//         <Human name={this.state.humans[2].name} age={this.state.humans[2].age}></Human>
+//         <button onClick={this.iqChnageHandler}>Change Content</button>
+//       </>
+//     )
+//   }
+// }
+
+
+import React, { useState } from 'react'
 import Human from './Human/Human'
 
-import React, { Component } from 'react'
+const App = (props) => {
+  const [initialHumanState, setHumanState] = useState({
+    humans: [
+      { name: "A", age: 50 },
+      { name: "B", age: 50 },
+      { name: "C", age: 50 }
+    ]
+  });
 
-export default class App extends Component {
-
-
-  constructor(props) {
-    super(props)
-
-    this.state = {
+  const changeIQHandler = () => {
+    setHumanState({
       humans: [
-        { name: "A", age: 24 },
-        { name: "B", age: 44 },
-        { name: "C", age: 84 }
+        { name: "A", age: 50 },
+        { name: "B", age: 50 },
+        { name: "C", age: 50 }
       ]
-    }
-
-    this.handleEvent = this.handleEvent.bind(this)
+    });
   }
-
-  componentDidMount() {
-
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) { if (prevState.name !== this.state.name) { this.handler() } }
-
-  componentWillUnmount() {
-
-  }
-
-  // Prototype methods, Bind in Constructor (ES2015)
-  handleEvent() { }
-
-  // Class Properties (Stage 3 Proposal)
-  handler = () => {
-    this.setState(
-      {
-        humans: [
-          { name: "A", age: 24 },
-          { name: "B", age: 44 },
-          { name: "C", age: 84 }
-        ]
-      }
-    )
-  }
-
-  render() {
-    return (
-      <>
-        <Human name={this.state.humans[0].name} age={this.state.humans[0].age}></Human>
-        <Human name={this.state.humans[1].name} age={this.state.humans[1].age}></Human>
-        <Human name={this.state.humans[2].name} age={this.state.humans[2].age}></Human>
-        <button onClick={this.handler}>Change Content</button>
-      </>
-    )
-  }
+  return (
+    <div className="App">
+      <Human name={initialHumanState.humans[0].name} age={initialHumanState.humans[0].age}></Human>
+      <Human name={initialHumanState.humans[1].name} age={initialHumanState.humans[1].age}></Human>
+      <Human name={initialHumanState.humans[2].name} age={initialHumanState.humans[2].age}></Human>
+      <button onClick={changeIQHandler}>Chnage IQ</button>
+    </div>
+  )
 }
-
+export default App;
