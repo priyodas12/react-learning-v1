2d7915841361fecb15636d55e69ec10762ee7700
Priyobrato Das <priyodas12@gmail.com>
2021-02-23 13:23:07 +0530

    context

--- src/ContextAPI/userContext.js
+++ src/ContextAPI/userContext.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const userContext = React.createContext();
+const userContext = React.createContext("Infoys");
 
 const UserProvider = userContext.Provider;
 const UserConsumer = userContext.Consumer;
