bf02109d4967539564a5242f8dbd0bc49f6c93c5
Priyobrato Das <priyodas12@gmail.com>
2021-02-24 17:44:53 +0530

    hooks

--- src/Hooks/Counter.js
+++ src/Hooks/Counter.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Counter() {
 
   const [count, setCount] = useState(0);
-  console.log(setCount);
+  console.log(typeof setCount);
+
+  useEffect(() => {
+    // hadling side effects like this
+    document.title = `You Clicked ${count} times`;
+  });
 
   const incrementCount = () => {
     setCount(count + 1);
@@ -11,7 +16,7 @@ export default function Counter() {
 
   return (
     <div>
-      <button onClick={incrementCount}>{count}</button>
+      {/*<button onClick={incrementCount}>{count}</button>*/}
       <button onClick={() => setCount(count + 1)}>{count}</button>
     </div>
   )
